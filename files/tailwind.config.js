11cb9859e011e280676efd1b9c4e7a75dc76cfd1
Chandra Shekhar Yadav <chandrashekhary866@gmail.com>
2026-07-27 14:57:15 +0545

    Migrate portfolio to React Vite

---
+++ files/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#eef2ff',
+          100: '#e0e7ff',
+          200: '#c7d2fe',
+          300: '#a5b4fc',
+          400: '#818cf8',
+          500: '#6366f1',
+          600: '#5850ec',
+          700: '#4f46e5',
+          800: '#4338ca',
+          900: '#3730a3'
+        }
+      }
+    }
+  },
+  plugins: []
+}
